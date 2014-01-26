4e1a8459d7d2f247f8c8aaa2004ec0dcd6f682d5
Chao Wang <cwang8023@gmail.com>
2014-01-27 01:35:01 +0800

    fix bug when loading response file

--- lib/easy-http-mock.rb
+++ lib/easy-http-mock.rb
@@ -14,14 +14,18 @@ class EasyHTTPMock < Sinatra::Base
 
   class << self
     def start(config, options)
-      [:port, :addr].each {|key| set key, options[key] if options.key? key}
+      @mocking_config_file = config
+      config_sinatra(options)
       begin
         define_mock(parsing_configuration(config))
       rescue Exception => e
-        puts 'Got exception when define mock http service'
-        puts e
+        abort "Got exception when define mocked http service:\n#{e}"
       end
       run!
+    end
+
+    def config_sinatra(options)
+      [:port, :addr].each { |key| set key, options[key] if options.key? key }
     end
 
     def parsing_configuration(config_file)
@@ -35,8 +39,9 @@ class EasyHTTPMock < Sinatra::Base
     def define_mock(conf)
       conf.each do |service|
         requestConf, responseConf = service['request'], service['response']
+        base_path = File.dirname(@mocking_config_file)
         get requestConf.delete('path'), requestConf.inject({}) { |h, (k, v)| h[k] = [v]; h} do
-          File.read(File.dirname(__FILE__) + "/" + responseConf)
+          File.read(base_path + '/' + responseConf)
         end
       end
     end
